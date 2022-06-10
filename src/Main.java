8bc0e0c15b69405d153a6929d03cc5d27cdbf6b4
BloodyRanger <BloodyRanger@inbox.ru>
2022-06-10 22:40:48 +0300

    Second Task

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(new BinOps().sum("100", "101"));
 
-        Calculator calc = new Calculator();
-        System.out.println(calc.newFormula()
-                .addOperand(5)
-                .addOperand(4)
-                .calculate(Calculator.Operation.MULT)
-                .result()
-        );
-
-        Ints calcul = new IntsCalculator();
-        System.out.println(calcul.mult(5,4));
+        System.out.println(new BinOps().mult("100", "101"));
     }
 }
